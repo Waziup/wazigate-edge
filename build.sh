39cf2c47d3c71d29f711ef6ef34da77df7cba494
Felix Markwordt <24inchstreetbikesoldier@gmail.com>
2022-05-24 14:24:17 +0200

    changed build.sh to build wazigate(-egde) and dashboard

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 # export GO111MODULE="on"
 # go mod init; go mod tidy
 
-BIN="wazigate-edge"
+# Build wazigate-dashboard
+cd wazigate-dashboard
+npm i && npm run build
+cd ..
 
-go build -o $BIN .
+# Build wazigate(-edge)
+go build -ldflags "-s -w" -o wazigate .
